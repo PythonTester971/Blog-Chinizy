7ac74db499a79404188f1ac5ce1aca9b799f83f4
PythonTester <gdenisse971@outlook.fr>
2025-03-03 22:55:01 +0100

    Sélection des cards by categories

--- archives.php
+++ archives.php
@@ -1,23 +1,37 @@
 <?php include_once './components/header.php';
 require_once 'db/article-db.php';
 
-$articles = getArticlesWithCategory();
 
+if (isset($_POST['archives-categories']) && $_POST['archives-categories'] != NULL) {
+
+    $category_id = $_POST['archives-categories'];
+    $articles = getArticlesByCategory($category_id);
+} else {
+
+    $articles = getArticlesWithCategory();
+}
 ?>
 
-<h1>Archives</h1>
+<main>
+    <h1>Archives</h1>
 
-<div class="container">
-    <div class="row g-5">
+    <div class="container">
+        <div class="row g-5">
 
-        <?php foreach ($articles as $article): ?>
-            <div class="col-xl-3 col-lg-4">
-                <?php include './components/art-card.php' ?>
-            </div>
-        <?php endforeach; ?>
+            <?php
+
+            include_once './components/categorie-select.php';
+
+            foreach ($articles as $article):
+
+            ?>
+                <div class="col-xl-3 col-lg-4">
+                    <?php include './components/art-card.php' ?>
+                </div>
+            <?php endforeach; ?>
+        </div>
     </div>
-</div>
-
+</main>
 
 
 <?php include_once './components/footer.php' ?>
--- components/categorie-select.php
+++ components/categorie-select.php
@@ -1,10 +1,18 @@
-<div class="form-group">
-    <label for="exampleFormControlSelect1">Example select</label>
-    <select class="form-control" id="exampleFormControlSelect1">
-        <option>1</option>
-        <option>2</option>
-        <option>3</option>
-        <option>4</option>
-        <option>5</option>
-    </select>
-</div>
+<?php require 'db/categories-db.php';
+
+$categories = getCategoryNames();
+
+?>
+
+<form action="../archives.php" method="post">
+    <div class="form-group">
+        <label for="categories">Catégories</label>
+        <select class="form-control" id="archives-categories" name="archives-categories">
+            <option value="">Choisir une catégorie</option>
+            <?php foreach ($categories as $category): ?>
+                <option value="<?php echo $category['id'] ?>"><?php echo $category['label'] ?></option>
+            <?php endforeach; ?>
+        </select>
+    </div>
+    <button type="submit" class="btn btn-primary mb-2">Valider</button>
+</form>
